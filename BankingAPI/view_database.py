e7d634298b01960f5c93234e3cbffa4087f1d865
sidubazjhb024 <sidubazjhb024@student.wethinkcode.co.za>
2024-11-20 09:04:04 +0200

    added database viewer

---
+++ BankingAPI/view_database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+#connect to the sqlite database file
+conn=sqlite3.connect('banking.db')
+
+#cursor to execute sqlcommands
+cursor=conn.cursor()
+
+#display allaccounts
+print('Accounts Table:')
+cursor.execute('SELECT * FROM account')
+accounts=cursor.fetchall()
+if accounts:
+    for account in accounts:
+        print(f'ID: {account[0]}, Name: {account[1]}, Email: {account[2]}, Balance: {account[3]}')
+else:
+    print('No accounts found')
+print('\nTransactions Table: ')
+
+#display all transactiond
+cursor.execute('SELECT * FROM transaction')
+transactions= cursor.fetchall()
+if transactions:
+    for transaction in transactions:
+        print(f'ID: {transaction[0]}, Account ID: {transaction[1]}, Amount: {transaction[2]}, Type: {transaction[3]}')
+else:
+    print('No transactions found')
+
+#close the connection
+conn.close()            
